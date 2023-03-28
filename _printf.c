0842f80e47ad0105e4f2e946886b85278583e97b
leonaekina <leonaekina@gmail.com>
2023-03-28 05:10:21 -0700

    Printf.c

--- _printf.c
+++ _printf.c
@@ -1,26 +1,39 @@
 #include "main.h"
 
-// Helper function to find the specifier function for a given specifier
+/**
+ * find_function - helper function to find the corresponding specifier function
+ * @specifiers: string of supported conversion specifiers
+ * @functions: array of pointer to corresponding conversion specifier functions
+ * @c: character representing a conversion specifier
+ * Return: pointer to conversion specifier function or NULL if not supported
+ */
+
 print_func find_function(const char *specifiers, print_func *functions, char c)
 {
 	for (int i = 0; i < strlen(specifiers); i++)
 	{
 		if (specifiers[i] == c)
 		{
-			return functions[i];
+			return (functions[0]);
 		}
 	}
-	return NULL;
+	return ((print_func)NULL);
 }
-// The main _printf function
+
+/**
+ *_printf - prints formatted output to stdout
+ *@format: format string containing conversion specifiers
+ *@...: variable arguments corresponding to conversion specifiers
+ *Return: the number of characters printed
+ */
+
 int _printf(const char *format, ...)
 {
 	va_list args;
 
 	va_start(args, format);
 
-	const char *specifiers = "cds%"; // List of supported conversion specifiers
-	
+	const char *specifiers = "cds%";
 	print_func functions[] = {&print_char, &print_int,
 		&print_string, &print_percent};
 
@@ -31,21 +44,25 @@ int _printf(const char *format, ...)
 		if (*p == '%')
 		{
 			p++;
+			print_func fn = find_function(specifiers, functions, *p);
 
- 			print_func fn = find_function(specifiers, functions, *p);
 			if (fn != NULL)
 			{
 				fn(args);
-    			} else {
-        			putchar('%');
+			}
+			else
+			{
+				putchar('%');
 				putchar(*p);
 				printed_chars += 2;
-				}
-		} else {
-			putchar(*p);
-			printed_chars++;
+			}
+		}
+			else
+			{
+				putchar(*p);
+				printed_chars++;
 			}
 	}
 	va_end(args);
-	return printed_chars;
+	return (printed_chars);
 }
